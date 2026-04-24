b75fda2a907692fff0ecf4e4c9a3d2509990bc42
SergioMoschini <moschini2002@gmail.com>
2026-04-24 12:59:52 +0200

    aggiunte alle repositories
    findbyutente findbyevento findbyutenteedevento e booleano existsbyemail

--- src/main/java/org/example/u5w3d5compito/repositories/PrenotazioniRepository.java
+++ src/main/java/org/example/u5w3d5compito/repositories/PrenotazioniRepository.java
@@ -1,9 +1,14 @@
 package org.example.u5w3d5compito.repositories;
-
 import org.example.u5w3d5compito.entities.Prenotazione;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
 import java.util.UUID;
 
+
 public interface PrenotazioniRepository extends JpaRepository <Prenotazione, UUID> {
+    List<Prenotazione> findByUtenteEdEvento(UUID eventoId, UUID utenteId);
+    List<Prenotazione> findAllByEvento(UUID eventoId);
+    Page<Prenotazione> findAllByUtente(UUID utenteId, Pageable pageable);
 }
--- src/main/java/org/example/u5w3d5compito/repositories/UtentiRepository.java
+++ src/main/java/org/example/u5w3d5compito/repositories/UtentiRepository.java
@@ -7,5 +7,6 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface UtentiRepository extends JpaRepository <Utente, UUID> {
+    boolean existsByEmail(String email);
     Optional<Utente> findByEmail(String email);
 }
