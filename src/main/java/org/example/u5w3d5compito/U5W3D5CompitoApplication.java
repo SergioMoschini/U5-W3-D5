ecd5087ad02cba369f8830f29b20cc331a39a157
SergioMoschini <moschini2002@gmail.com>
2026-04-24 11:53:21 +0200

    first commit

---
+++ src/main/java/org/example/u5w3d5compito/U5W3D5CompitoApplication.java
@@ -0,0 +1,13 @@
+package org.example.u5w3d5compito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W3D5CompitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(U5W3D5CompitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/u5w3d5compito/U5W3D5CompitoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.u5w3d5compito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W3D5CompitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
